789682ba1c0ea97183af81fe47be2dd28f4a1ab0
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2026-06-02 20:47:48 -0600

    yes

--- GAS/practica_3/main.py
+++ GAS/practica_3/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt 
 import pandas as pd 
 df=pd.read_csv("data.csv")
-df.columns=[2,4,8,16]
+df.columns=["unico","doble","uniforme"]
 df.plot(kind="box")
 plt.title("Comparacion: torneo")
 plt.show()
